9aa0c90319528fb02c0f7f14c271c4222187364b
wwagner4 <wwagner4@gmail.com>
2018-04-23 23:39:17 +0200

    added image transform

--- transform_images.py
+++ transform_images.py
@@ -1,15 +1,26 @@
 from os import listdir
+from os.path import join
+
+from wand.image import Image
 
 
 def is_jpg_file(file_name: str):
     return file_name.upper().endswith('JPG')
 
 
+def transform(di: str, fi: str):
+    base = fi[:-4]
+    print("transforming image '{}' base '{}' in {}".format(fi, base, di))
+    with Image(filename=join(di, fi)) as img:
+        img.format = 'png'
+        img.save(filename=join(di, base + ".png"))
+
+
+
 def transform_images(di: str):
-    print("transforming images in '{}'".format(di))
-    onlyfiles = [f for f in listdir(di) if is_jpg_file(f)]
-    for file in onlyfiles:
-        print("File: '{}'".format(file))
+    jpg_images = [f for f in listdir(di) if is_jpg_file(f)]
+    for file in jpg_images:
+        transform(di, file)
 
 
 transform_images("/Users/wwagner4/Pictures/Diverses/Wolfis Experimente/Green Screen/100")

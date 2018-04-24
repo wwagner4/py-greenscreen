990d87648f1cbba54b0a4a1cc62a96ff614d2426
wwagner4 <wwagner4@gmail.com>
2018-04-24 08:20:05 +0200

    refactoring

--- transform_images.py
+++ transform_images.py
@@ -1,7 +1,7 @@
-from os import *
-from os.path import join
+import os
+import os.path as osp
 
-from wand.image import Image
+import wand.image as wim
 
 
 def is_jpg_file(file_name: str):
@@ -17,41 +17,41 @@ def transform_file(di: str, fi: str):
     fipng = base + ".png"
     print("transforming {} --> {}".format(fi, fipng))
 
-    with Image(filename=join(di, fi)) as img:
+    with wim.Image(filename=osp.join(di, fi)) as img:
         img.format = 'png'
-        img.save(filename=join(di, fipng))
+        img.save(filename=osp.join(di, fipng))
 
 
 def scale_file(di: str, fi: str):
     print("scaling {}".format(fi))
 
-    with Image(filename=join(di, fi)) as img:
+    with wim.Image(filename=osp.join(di, fi)) as img:
         k = 100.0 / img.height
         w1 = int(k * img.width)
         h1 = int(k * img.height)
         print("resize {} to {} x {}".format(fi, w1, h1))
         img.resize(w1, h1)
-        img.save(filename=join(di, fi))
+        img.save(filename=osp.join(di, fi))
 
 
 def transform_dir(di: str):
-    jpg_images = [f for f in listdir(di) if is_jpg_file(f)]
+    jpg_images = [f for f in os.listdir(di) if is_jpg_file(f)]
     for file in jpg_images:
         transform_file(di, file)
 
 
 def scale_dir(di: str):
-    images = [f for f in listdir(di) if is_png_file(f)]
+    images = [f for f in os.listdir(di) if is_png_file(f)]
     for file in images:
         scale_file(di, file)
 
 
 def deletejpg_dir(di: str):
-    images = [f for f in listdir(di) if is_jpg_file(f)]
+    images = [f for f in os.listdir(di) if is_jpg_file(f)]
     for file in images:
-        fn = join(di, file)
+        fn = osp.join(di, file)
         print("deleting {}".format(fn))
-        remove(fn)
+        os.remove(fn)
 
 
 def transform_images(di: str):

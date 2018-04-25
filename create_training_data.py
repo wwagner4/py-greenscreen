a6527d2d21905e18c51a3a2be748436807c55a75
wwagner4 <wwagner4@gmail.com>
2018-04-25 23:25:25 +0200

    added multiple around indices

--- create_training_data.py
+++ create_training_data.py
@@ -1,6 +1,6 @@
 import itertools as it
 import os.path as osp
-from typing import Tuple, Iterable
+from typing import Tuple, Iterable, List
 
 import matplotlib.pylab as pl
 import numpy as np
@@ -12,7 +12,7 @@ def flatmap(f, list_of_list):
     return it.chain.from_iterable(map(f, list_of_list))
 
 
-def create_training_img_arrays(img_name: str) -> Iterable[Tuple[np.array, np.array]]:
+def create_training_img_arrays(img_name: str) -> Tuple[np.array, np.array]:
     fnamg = "bsp{}_0.png".format(img_name)
     fnamt = "bsp{}_1.png".format(img_name)
     img: np.array = pl.imread(osp.join(imgDir, fnamg))
@@ -26,34 +26,61 @@ def core_indices(rows: int, cols: int, delta: int) -> Iterable[Tuple[int, int]]:
             yield (i, j)
 
 
-def square_indices(delta: int) -> Iterable[Tuple[int, int]]:
+def square_indices_rows(delta: int) -> Iterable[Tuple[int, int]]:
     for i in range(-delta, delta + 1):
         for j in range(-delta, delta + 1):
             yield (i, j)
 
 
-def create_training_data(name: str, delta: int) -> Iterable[np.array]:
-    g, t = create_training_img_arrays(name)
-    rows = g.shape[0]
-    cols = g.shape[1]
-    ci = core_indices(rows, cols, delta)
-    for r, c in ci:
-        transp = t[r, c, 0]
+def square_indices_cols(delta: int) -> Iterable[Tuple[int, int]]:
+    for i in range(-delta, delta + 1):
+        for j in range(-delta, delta + 1):
+            yield (j, i)
+
+
+def square_indices_rows_cols(delta: int) -> Iterable[Tuple[int, int]]:
+    return flatmap(lambda l: l, [square_indices_rows(delta), square_indices_cols(delta)])
+
+
+def create_training_data(name: str, delta: int, around_idxs: List[Tuple[int, int]]) -> Iterable[np.array]:
+    green_img, transp_img = create_training_img_arrays(name)
+    rows = green_img.shape[0]
+    cols = green_img.shape[1]
+    core_idxs: Iterable[Tuple[int, int]] = core_indices(rows, cols, delta)
+    return create_training_data1(green_img, transp_img, core_idxs, around_idxs)
+
+
+def create_training_data1(
+        g: np.array, t: np.array,
+        core_idxs: Iterable[Tuple[int, int]], around_idxs: List[Tuple[int, int]]) -> Iterable[np.array]:
+    for r, c in core_idxs:
         greens = np.empty(0, dtype=float)
-        for i, j in square_indices(delta):
+        for i, j in around_idxs:
             r1 = r + i
             c1 = c + j
             green = g[r1, c1]
             greens = np.hstack((greens, green))
+        transp = t[r, c, 0]
         yield np.hstack((greens, transp))
 
 
-names = ['1', '2']
-datas = flatmap(lambda name: create_training_data(name, 10), names)
+def run():
+    names = ['1', '2']
+    delta = 10
+    around_idxs = list(square_indices_rows_cols(delta))
+    datas = flatmap(lambda name: create_training_data(name, delta, around_idxs), names)
+    for idx, line in enumerate(datas):
+        print("{} {}".format(idx, line))
 
-for idx, line in enumerate(datas):
-    print("{} {}".format(idx, line))
 
+def test():
+    si = square_indices_rows_cols(1)
+    for i in si:
+        print(i)
+
+
+# test()
+run()
 
 '''
 for idx, line in enumerate(create_training_data('1', 10)):

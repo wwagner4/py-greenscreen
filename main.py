81e8839977eb1ef04771790021b532704e90a78b
wwagner4 <wwagner4@gmail.com>
2018-05-10 19:56:56 +0200

    full execution

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import common as co
+import create_training_data as ctd
+import train_model as tm
 import use_model as um
 
 out_file = co.work_file('data-img100.csv')
 model_file = co.work_file("model-img100.h5")
 
-# ctd.create(out_file)
-# tm.train(out_file, model_file)
+ctd.create(out_file)
+tm.train(out_file, model_file)
 um.use(model_file)

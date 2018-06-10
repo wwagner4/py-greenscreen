36edbaeaae089d9e9d982157a790701f8951600d
wwagner4 <wwagner4@gmail.com>
2018-06-10 23:27:26 +0200

    fix config

--- main_cfg_template.py
+++ main_cfg_template.py
@@ -1,5 +1,6 @@
 # work_dir = "/root/work/work-greenscreen"
-# work_dir = "???"
+# root_dir = "/root/prj/py-greenscreen"
+
 # work_dir = "/Users/wwagner4/work/work-greenscreen"
 # root_dir = "/Users/wwagner4/prj/py/greenscreen"
 
